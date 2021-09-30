240a000e367b249c9b1a465830215a7d3513ad7e
Atandra Bharati <atandrabharati@gmail.com>
2021-09-30 10:31:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,31 +15,31 @@ document.addEventListener("keydown", function(event) {
 function decision(letter) {
     switch (letter) {
         case "w":
-            let audio1 = new Audio("sounds/tom-0.mp3");
+            let audio1 = new Audio("./tom-0.mp3");
             audio1.play();
             break;
         case "a":
-            let audio2 = new Audio("sounds/tom-1.mp3");
+            let audio2 = new Audio("./tom-1.mp3");
             audio2.play();
             break;
         case "s":
-            let audio3 = new Audio("sounds/tom-2.mp3");
+            let audio3 = new Audio("./tom-2.mp3");
             audio3.play();
             break;
         case "d":
-            let audio4 = new Audio("sounds/tom-3.mp3");
+            let audio4 = new Audio("./tom-3.mp3");
             audio4.play();
             break;
         case "j":
-            let audio5 = new Audio("sounds/tom-4.mp3");
+            let audio5 = new Audio("./tom-4.mp3");
             audio5.play();
             break;
         case "k":
-            let audio6 = new Audio("sounds/tom-5.mp3");
+            let audio6 = new Audio("./tom-5.mp3");
             audio6.play();
             break;
         case "l":
-            let audio7 = new Audio("sounds/tom-6.mp3");
+            let audio7 = new Audio("./tom-6.mp3");
             audio7.play();
             break;
         default:
